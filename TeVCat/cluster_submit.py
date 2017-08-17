f23f68a90ca27444a008344adb904d605607d790
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2017-08-17 14:50:34 -0500

    Adding clust submission and plot of background trials

---
+++ TeVCat/cluster_submit.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os, sys, argparse
+
+if __name__ == "__main__":
+
+    p = argparse.ArgumentParser(
+            description='Run stacking test trials on cluster',
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+    p.add_argument('--test', dest='test', action='store_true',
+            default=False,
+            help='Option for running test off cluster')
+    p.add_argument('--nJobs', dest='nJobs', type=int,
+            default='100',
+            help='max jobs running on cluster')
+    p.add_argument('--nTrials', dest='nTrials', type=int,
+            default='10',
+            help='max jobs running on cluster')
+    p.add_argument('--maxjobs', dest='maxjobs',
+            default='1200',
+            help='max jobs running on cluster')
+    p.add_argument('--rm_old', dest='rm_old', action='store_true',
+            default=False,
+            help='Remove old dag files?')
+    p.add_argument('--rescue', dest='rescue', action='store_true',
+            default=False,
+            help='Option for submitting rescue file')
+    args = p.parse_args()
+
+    script = "/home/zgriffith/photon_analysis/pev_photons/TeVCat/stacking_test.py"
+
+    if args.test:
+        cmd = 'python '+script 
+    else:
+        dag_name = '/data/user/zgriffith/dagman/myJobs/stacking_trials.dag'
+        ex       = 'condor_submit_dag -f -maxjobs ' +args.maxjobs+' '+dag_name
+
+        if args.rescue:
+            os.system(ex)
+            sys.exit()
+
+        if args.rm_old:
+            print('Deleting '+dag_name[:-3]+' files...')
+            os.system('rm '+dag_name[:-3]+'*')
+
+        dag = open(dag_name, "w+")
+
+    index = 0
+    for job in range(args.nJobs):
+        arg  = ' --runTrial --job %s --nTrials %s' % (job, args.nTrials)
+        if args.test:
+            ex  = ' '.join([cmd, arg])
+        else:
+            arg = script+arg
+            dag.write("JOB " + str(index) + " /data/user/zgriffith/dagman/OneJob.submit\n")
+            dag.write("VARS " + str(index) + " ARGS=\"" + arg + "\"\n")
+        index += 1
+    os.system(ex)
