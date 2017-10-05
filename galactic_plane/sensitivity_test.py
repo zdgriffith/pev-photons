e5fef8599456e229e2b36505081518aea3c3acd0
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2017-10-05 09:00:06 -0500

    New skylab sensitvity calculation

--- galactic_plane/sensitivity_test.py
+++ galactic_plane/sensitivity_test.py
@@ -5,39 +5,30 @@
 ########################################################################
 
 import re
+import sys
 import argparse as ap
 import numpy as np
 import pandas as pd
 
+from skylab_comp.sensitivity_utils import estimate_sensitivity
 from support_pandas import livetimes
 from skylab_comp.template import Template
 from skylab_comp.template_llh import TemplateLLH, MultiTemplateLLH
 from skylab_comp.template_injector import TemplateInjector
 from skylab_comp.llh_models import ClassicLLH, EnergyLLH
 
-if __name__ == "__main__":
+def sensitivity(args):
+    sinDec_range = [-1, -0.8]
+    sinDec_bins = np.arange(-1.0, -0.799, 0.01)
+    energy_range = [5.7, 8.0]
+    energy_bins = [np.linspace(5.7,8,20), sinDec_bins]
 
-    p = ap.ArgumentParser(description='Test galactic plane sensitivity.',
-                          formatter_class=ap.RawDescriptionHelpFormatter)
-    p.add_argument('--prefix', type=str,
-                   default='/data/user/zgriffith/pev_photons/',
-                   help='Base directory for file storing.')
-    p.add_argument('--name', type=str, default='fermi_pi0',
-                   help='The name of the template.')
-    p.add_argument("--alpha", type=float, default=3.0,
-                   help='Spectral index of signal.')
-    args = p.parse_args()
-
-    sinDec_range = [-1.,-0.8]
-    sinDec_bins  = np.linspace(-1., -0.80, 21)
-    energy_range = [5.5,8.5] # log(E/GeV)
-    energy_bins  = [np.linspace(5.5,8.5,30), sinDec_bins]
-
-    mc_years  = dict()
-    lt_years  = dict()
+    mc_years = dict()
+    exp_years = dict()
+    lt_years = dict()
     llh_model = dict()
     template_years = dict()
-    template_llh = MultiTemplateLLH()
+    template_llh = MultiTemplateLLH(ncpu=10)
 
     years = ['2011', '2012', '2013', '2014','2015']
     for i, year in enumerate(years): 
@@ -51,11 +42,13 @@ if __name__ == "__main__":
         template_years[i] = template
         lt_years[i] = livetime 
         mc_years[i] = mc 
+        exp_years[i] = exp
 
         llh_model[year] = EnergyLLH(twodim_bins=energy_bins,
                                     twodim_range=[energy_range,sinDec_range],
                                     sinDec_bins=sinDec_bins,
                                     sinDec_range=sinDec_range,
+                                    fix_index=True,
                                     bounds=[args.alpha, args.alpha],
                                     seed=1)
 
@@ -68,19 +61,29 @@ if __name__ == "__main__":
 
     inj = TemplateInjector(template=template_years,
                            gamma=args.alpha,
-                           E0=1000,
+                           E0=2*10**6,
                            Ecut=None,
                            seed=1)
-    inj.fill(exp, mc_years, lt_years)
+    inj.fill(exp_years, mc_years, lt_years)
 
-    ab = [[0.5],[0.9]] # 90 percent upper limit.
-    result, trials, weights = template_llh.weighted_sensitivity(alpha=ab[0],
-                                                                beta=ab[1],
-                                                                inj=inj,
-                                                                #mc=mc_years,
-                                                                eps=1.e-2,
-                                                                n_bckg=10000,
-                                                                n_iter=1000)
+    path = ("/home/zgriffith/public_html/"
+            "photon_analysis/pev_photons/galactic_plane/"+args.name+"/")
+    results = estimate_sensitivity(template_llh, inj,
+                                   nstep=11, 
+                                   ni_bounds=[0,2000], 
+                                   nsample=100, 
+                                   path=path)
 
-    print(result['flux'][0])
-    np.save(args.prefix+args.name+'_sensitivity.npy', result['flux'][0])
+if __name__ == "__main__":
+    p = ap.ArgumentParser(description='Test galactic plane sensitivity.',
+                          formatter_class=ap.RawDescriptionHelpFormatter)
+    p.add_argument('--prefix', type=str,
+                   default='/data/user/zgriffith/pev_photons/',
+                   help='Base directory for file storing.')
+    p.add_argument('--name', type=str, default='fermi_pi0',
+                   help='The name of the template.')
+    p.add_argument("--alpha", type=float, default=3.0,
+                   help='Spectral index of signal.')
+    args = p.parse_args()
+
+    sensitivity(args)
