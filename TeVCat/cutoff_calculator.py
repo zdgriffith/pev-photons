82ed4c439bc9072c746ada4394e011ce8e5af4fe
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2017-10-05 08:31:21 -0500

    pep8 corrections

--- TeVCat/cutoff_calculator.py
+++ TeVCat/cutoff_calculator.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python
 
-#==============================================================================
-# File Name     : cutoff_calculator.py
-# Description   : For each HESS source, calculate the min cut-off energy from extrapolation
-# Creation Date : 09-12-2017
-# Last Modified : Thu 14 Sep 2017 02:15:22 PM CDT
-# Created By    : Zach Griffith 
-#==============================================================================
+########################################################################
+# For each HESS source, calculate the
+# minimum cut-off energy matching a sensitivity
+########################################################################
 
-import argparse, sys, os, simFunctions
+import argparse
 import numpy as np
-sys.path.append(os.path.expandvars("$HOME"))
-import dashi
-dashi.visual()
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+import dashi
 from support_pandas import get_fig_dir
 from scipy.interpolate import InterpolatedUnivariateSpline
+
+dashi.visual()
 plt.style.use('stefan')
 fig_dir = get_fig_dir()
 colors = mpl.rcParams['axes.color_cycle']
 
-
 def cutoff_calculator(args):
-    sources   = np.load(args.prefix+'TeVCat/hess_sources.npz')
+    sources = np.load(args.prefix+'TeVCat/hess_sources.npz')
 
-    surv   = np.loadtxt(args.prefix+'TeVCat/gamma_survival_vs_distance.txt')
-    surv   = surv.T
+    surv = np.loadtxt(args.prefix+'TeVCat/gamma_survival_vs_distance.txt')
+    surv = surv.T
     spline = InterpolatedUnivariateSpline(surv[0], surv[1], k=2)
-    ratio  = spline(sources['distance'])
+    ratio = spline(sources['distance'])
 
-    medians  = ratio*(sources['flux']*1e-12) # Survival Probability x flux at 1 TeV
-    medians *= 1000**(2-sources['alpha'])     # Extrapolation to 1 PeV
+    medians = ratio*(sources['flux']*1e-12)
+    medians *= 1000**(2-sources['alpha'])
 
-    kind   = ['sensitivity','discovery_potential']
+    kind = ['sensitivity', 'discovery_potential']
     cutoff = [[],[]] 
 
     for j, k in enumerate(kind):
@@ -52,23 +49,23 @@ def cutoff_calculator(args):
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(
-            description='calculate cut-off energies',
+            description='Calculate minimum cut-off energies for sources.',
             formatter_class=argparse.RawDescriptionHelpFormatter)
-    p.add_argument('--prefix', dest='prefix', type = str,
+    p.add_argument('--prefix', type=str,
                    default = '/data/user/zgriffith/pev_photons/',
-                   help    = 'base directory for file storing')
-    p.add_argument('--outFile', dest='outFile', type = str,
-                   default = 'cutoff_energies.png',
-                   help    = 'file name')
+                   help='The base directory for file storing.')
+    p.add_argument('--outFile', type=str,
+                   default='cutoff_energies.png',
+                   help='The output file name.')
     args = p.parse_args()
 
     cutoff = cutoff_calculator(args)
-
-    kind   = ['Sensitivity', 'Discovery Potential']
+    kind = ['Sensitivity', 'Discovery Potential']
     
     for i, k in enumerate(kind):
         hist = dashi.factory.hist1d(cutoff[i], np.arange(0,3025,25))
         hist.line(label=k) 
+
     plt.xlabel('Minimum Cut-Off Energy [TeV]')
     plt.legend()
     plt.savefig(fig_dir+args.outFile, facecolor='none', dpi=300)
