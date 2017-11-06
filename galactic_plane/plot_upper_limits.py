0c6f9755aeb5fc7d8af28e6e344cd60712a8db34
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2017-11-06 10:19:18 -0600

    Changing comparison to be angular integrated to be consistent

--- galactic_plane/plot_upper_limits.py
+++ galactic_plane/plot_upper_limits.py
@@ -22,18 +22,6 @@ def plot_limit(x,y, unc = None, label = '', color = 'k'):
                 color=color, marker="H",
                 label=label)
 
-    # A vertical line.
-    plt.plot([x, x], [y, 0.33*y],
-             zorder=0,
-             color=color, linewidth=1.5,
-             alpha=0.75)
-
-    # The bottom triangle
-    plt.scatter(x, 0.33*y,
-                zorder=2, s=80, 
-                color=color, marker="v",
-                alpha=0.75)
-
     if unc:
         plt.errorbar(x, y, yerr=unc*y,
                      color=color,
@@ -42,9 +30,9 @@ def plot_limit(x,y, unc = None, label = '', color = 'k'):
 if __name__ == "__main__":
     
     x = 2*10**6
-    conv = (x**2)/(2*np.pi*0.2)
-    fluxes = {'Old Skylab (broken)': 3.79e-9,
-              'Old Skylab (fixed)': 6.63665400875e-10,
+    conv = (x**2)
+    fluxes = {'Old Skylab (broken)': 3.83e-9,
+              'Old Skylab (fixed)': 8.33986539132e-10,
               'Fermi pi$^0$': (2.47e-22)*conv,
               '5$^{\circ}$ box': (3.42e-22)*conv,
               'Ingelman-Thunman': (1.54e-22)*conv}
@@ -58,7 +46,7 @@ if __name__ == "__main__":
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel(r'$E_\gamma$ [GeV]')
-    plt.ylabel(r'$E^2J_\gamma$ [GeV cm${}^{-2}$ s${}^{-1}$ sr${}^{-1}$]')
+    plt.ylabel(r'$E^2J_\gamma$ [GeV cm${}^{-2}$ s${}^{-1}$]')
     plt.legend()
     plt.savefig(fig_dir+'sensitivity_comparison.png', facecolor='none',
                 bbox_inches="tight", dpi=300)
