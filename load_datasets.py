15a3f413993d35d291e841d0fc822c4d472eab02
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2017-12-18 12:56:51 -0600

    return only the llh object, other objects are members

--- load_datasets.py
+++ load_datasets.py
@@ -4,41 +4,61 @@
 # Functions for loading Skylab objects.
 ########################################################################
 
+import numpy as np
+
 from skylab.datasets import Datasets
 from skylab.llh_models import EnergyLLH
+
+def load_ps_dataset(args):
+    from skylab.ps_llh import PointSourceLLH, MultiPointSourceLLH
+
+    ps_llh = MultiPointSourceLLH(ncpu=args.ncpu, seed=args.seed)
+
+    years = ['2011', '2012', '2013', '2014', '2015']
+
+    for i, year in enumerate(years): 
+        name = 'IC86.'+year
+        exp, mc, livetime = Datasets['GammaRays5yr_PointSrc'].season(name)
+        energy_bins = Datasets['GammaRays5yr_PointSrc'].energy_bins(name)
+        energy_range = [energy_bins[0], energy_bins[-1]]
+        sinDec_bins = Datasets['GammaRays5yr_PointSrc'].sinDec_bins(name)
+        sinDec_range = [sinDec_bins[0], sinDec_bins[-1]]
+
+        llh_model = EnergyLLH(twodim_bins=[energy_bins, sinDec_bins],
+                              twodim_range=[energy_range, sinDec_range],
+                              sinDec_bins=sinDec_bins,
+                              sinDec_range=sinDec_range)
+
+        year_psllh = PointSourceLLH(exp, mc, livetime,
+                                    ncpu=args.ncpu,
+                                    mode='box',
+                                    scramble=True,
+                                    llh_model=llh_model,
+                                    delta_ang=np.radians(10*0.4))
+
+        ps_llh.add_sample(year, year_psllh)
+
+    return ps_llh
 
 def load_gp_dataset(args):
     from skylab.template import Template
     from skylab.template_llh import TemplateLLH, MultiTemplateLLH
 
-    sinDec_range = [-1, -0.8]
-    energy_range = [5.7, 8.0]
-
-    template_llh = MultiTemplateLLH(ncpu=args.ncpu)
-    template_years = dict()
-    exp_years = dict()
-    mc_years = dict()
-    lt_years = dict()
-    llh_model = dict()
-
-    years = ['2011', '2012', '2013', '2014','2015']
+    years = ['2011', '2012', '2013', '2014', '2015']
 
     for i, year in enumerate(years): 
         name = 'IC86.'+year
         exp, mc, livetime = Datasets['GammaRays5yr_GalPlane'].season(name)
-        sinDec_bins = Datasets['GammaRays5yr_GalPlane'].sinDec_bins(name)
         energy_bins = Datasets['GammaRays5yr_GalPlane'].energy_bins(name)
-        lt_years[i] = livetime 
-        mc_years[i] = mc 
-        exp_years[i] = exp
+        energy_range = [energy_bins[0], energy_bins[-1]]
+        sinDec_bins = Datasets['GammaRays5yr_GalPlane'].sinDec_bins(name)
+        sinDec_range = [sinDec_bins[0], sinDec_bins[-1]]
 
         template = Template((args.prefix+'/galactic_plane/'+year+
                              '/'+args.name+'_exp.npy'), reduced=True)
-        template_years[i] = template
 
         llh_model[year] = EnergyLLH(twodim_bins=[energy_bins, sinDec_bins],
                                     twodim_range=[energy_range, sinDec_range],
-                                    seed=1,
                                     bounds=[args.alpha, args.alpha],
                                     fix_index=True,
                                     sinDec_bins=sinDec_bins,
@@ -52,4 +72,4 @@ def load_gp_dataset(args):
 
         template_llh.add_sample(year, year_llh)
 
-    return template_llh, exp_years, mc_years, lt_years
+    return template_llh
