4288ecc8a13c51b396de8696474a590d89d1a95f
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2017-12-19 11:29:53 -0600

    consolidating the scan methods

--- all_sky/all_sky_scan.py
+++ all_sky/all_sky_scan.py
@@ -5,21 +5,32 @@
 ########################################################################
 
 import argparse
-import matplotlib
 import numpy as np
-import matplotlib.pyplot as plt
-
 import logging
 
-from support_pandas import get_fig_dir, livetimes
-from skylab.ps_llh import PointSourceLLH, MultiPointSourceLLH
-from skylab.llh_models import ClassicLLH, EnergyLLH
+import healpy as hp
 
-logging.getLogger("skylab.ps_llh.PointSourceLLH").setLevel(logging.INFO)
+from load_datasets import load_ps_dataset
 
-fig_dir = get_fig_dir()
-plt.style.use('mystyle')
-colors = matplotlib.rcParams['axes.color_cycle']
+def manual_scan(ps_llh, args):
+    npix = hp.nside2npix(args.nside)
+    m = np.zeros(npix)
+
+    theta, ra = hp.pix2ang(args.nside, range(npix))
+    dec = np.pi/2. - theta
+    mask = np.less(np.sin(dec), -0.8)
+
+    ts = np.zeros(npix, dtype=np.float)
+    xmin = np.zeros_like(ts, dtype=[(p, np.float) for p in ps_llh.params])
+    if args.extension:
+        ts, xmin = ps_llh._scan(ra[mask], dec[mask],
+                                ts, xmin, mask,
+                                src_extension=np.radians(args.extension))
+        np.save(args.prefix+'/all_sky/ext/skymap_ext_%s.npy' % args.extension,
+                ts)
+    else:
+        ts, xmin = ps_llh._scan(ra[mask], dec[mask], ts, xmin, mask)
+        np.save(args.prefix+'/all_sky/skymap.npy', ts)
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(
@@ -31,41 +42,32 @@ if __name__ == "__main__":
     p.add_argument('--outFile', type=str,
                    default='all_sky/skymap.npy',
                    help='The output file name.')
+    p.add_argument("--ncpu", type=int, default=1,
+                    help="Number of cores to run on.")
+    p.add_argument("--seed", type=int, default=1,
+                   help='rng seed')
+    p.add_argument("--nside", type=int, default=512,
+                   help='The healpix resolution.')
+    p.add_argument('--extension', type=float, default=0,
+                   help='Spatial extension to source hypothesis in degrees.')
+    p.add_argument('--coarse_scan', action='store_true', default=False,
+                   help=('Use the all_sky_scan() method, which runs'
+                         'a coarser scan first, then a follow up'))
     args = p.parse_args()
 
-    dec_bins = np.linspace(-1., -0.8, 21)
-    energy_bins = [np.linspace(5.7,8,24), dec_bins]
+    # Load the dataset.
+    ps_llh = load_ps_dataset(args)
 
-    # Initialization of multi-year LLH object.
-    psllh = MultiPointSourceLLH(ncpu=20)
-    tot_mc = dict()
-    llh_model = dict()
+    # Set the logging output file.
+    logging.getLogger("skylab.ps_llh.PointSourceLLH").setLevel(logging.INFO)
+    logging.basicConfig(filename=args.prefix+'all_sky/scan.log',
+                        filemode='w', level=logging.INFO)
 
-    years = ['2011', '2012', '2013', '2014','2015']
-    for i, year in enumerate(years): 
-        livetime = livetimes(year)*1.157*10**-5  # Convert seconds to days.
-        exp = np.load(args.prefix+'/datasets/'+year+'_exp_ps.npy')
-        mc  = np.load(args.prefix+'/datasets/'+year+'_mc_ps.npy')
-
-        llh_model[year] = EnergyLLH(twodim_bins=energy_bins,
-                                    twodim_range=[[5.7,8],[-1,-0.8]],
-                                    sinDec_bins=dec_bins,
-                                    sinDec_range=[-1,-0.8])
-
-        year_psllh = PointSourceLLH(exp, mc, livetime,
-                                    ncpu=20,
-                                    mode='box',
-                                    scramble=False,
-                                    llh_model=llh_model[year],
-                                    delta_ang=np.radians(10*0.4))
-
-        psllh.add_sample(year, year_psllh)
-        tot_mc[i] = mc 
-
-    for i, scan in enumerate(psllh.all_sky_scan()):
-        if i > 0:
-            m = scan[0]['TS']
-            break
-
-    # Stores the skymap in a standard directory.
-    np.save(args.prefix+args.outFile, m)
+    if args.coarse_scan:
+        for i, scan in enumerate(psllh.all_sky_scan()):
+            if i > 0:
+                m = scan[0]['TS']
+                break
+        np.save(args.prefix+args.outFile, m)
+    else:
+        manual_scan(ps_llh, args)
