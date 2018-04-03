b522fdc474255119f640c9d09157a6248ff6212f
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2018-04-03 17:08:39 -0500

    generalizing to be able to test impact of any parameter shift

--- pev_photons/systematics/gamma_charge_test.py
+++ pev_photons/systematics/gamma_charge_test.py
@@ -85,6 +85,43 @@ def prediction(f, selection, year, cut_val):
 
     return f[cut]
 
+
+def plot_dists():
+
+    fracs = []
+    errors = []
+    for i, param_val in enumerate(args.param_range):
+        level3 = []
+        level4 = []
+        for year in args.years: 
+            gammas = pd.read_hdf(resource_dir+'datasets/level3/'+year+'_mc_quality.hdf5')
+            gammas[args.param] = param_val*gammas[args.param]
+            passing_gammas = prediction(gammas, args.selection,
+                                        year, args.cut_val)
+            level3.append(gammas)
+            level4.append(passing_gammas)
+
+        E_bins, frac, error = plot_fraction(pd.concat(level3), pd.concat(level4),
+                                            label='{:.2}*{}'.format(param_val, args.param_name))
+        fracs.append(frac)
+        errors.append(error)
+        if param_val == 1.0:
+            standard_index = i
+
+    plot_uncertainty(E_bins, fracs, errors, standard_index)
+
+
+def plot_uncertainty(E_bins, fracs, errors, standard_index):
+    for i, frac in enumerate(fracs):
+        ratio = frac/fracs[standard_index] 
+        ax1.step(E_bins, ratio, color=colors[i])
+    
+    ax1.fill_between(E_bins,
+                     1 - errors[standard_index]/fracs[standard_index],
+                     1 + errors[standard_index]/fracs[standard_index],
+                     step='pre', color=colors[standard_index], alpha=0.5)
+
+
 if __name__ == "__main__":
 
     p = argparse.ArgumentParser(
@@ -95,6 +132,16 @@ if __name__ == "__main__":
                    help    = 'base directory for file storing')
     p.add_argument('--cut_val', dest='cut_val', type = float,
                    default = 0.7, help = 'cut value for random forests')
+    p.add_argument('--param', type=str,
+                   default='s125', help='Parameter key')
+    p.add_argument('--param_name', type=str,
+                   default='S$_{125}$', help='Parameter name')
+    p.add_argument('--param_range', type=float, nargs='+',
+                   default=[1.15, 1.00, 0.85],
+                   help='Parameter values to test.')
+    p.add_argument('--years', type=str, nargs='+',
+                   default=['2011', '2012', '2013', '2014', '2015'],
+                   help='Year(s) to include.')
     p.add_argument('--selection', type=str,
                    default='point_source', help='event selection')
     args = p.parse_args()
@@ -105,35 +152,12 @@ if __name__ == "__main__":
     plt.rcParams['xtick.labelsize'] = 10
     plt.rcParams['ytick.labelsize'] = 10
 
-    years = ['2011', '2012', '2013', '2014', '2015']
-
     fig, (ax0, ax1) = plt.subplots(2, 1, gridspec_kw={'height_ratios':[3, 1]})
 
-    comp = []
-    for charge_fraction in [0.5, 1.0, 1.5]:
-        level3 = []
-        level4 = []
-        for i, year in enumerate(years): 
-            gammas = pd.read_hdf(resource_dir+'datasets/level3/'+year+'_mc_quality.hdf5')
-            level3.append(gammas)
-            gammas['charges'] = charge_fraction*gammas['charges']
-            passing_gammas = prediction(gammas, args.selection, year, args.cut_val)
-            level4.append(passing_gammas)
+    plot_dists()
 
-        x, vals, error = plot_fraction(pd.concat(level3), pd.concat(level4),
-                                       label='%.2f*Charge' % charge_fraction)
-        comp.append(vals)
-        if charge_fraction == 1.0:
-            base_error = error
-
-    for i, c in enumerate(comp):
-        ratio = c/comp[1] 
-        ax1.step(x, ratio, color=colors[i])
-    
-    ax1.fill_between(x, 1 - base_error/comp[1],
-                     1 + base_error/comp[1], step='pre',
-                     color=colors[1], alpha=0.5)
-
+    major_ticks = np.arange(6, 8.5, 0.5)
+    ax0.set_xticks(major_ticks)
     ax0.set_xticklabels([])
     ax0.set_xlim([5.7,8])
     ax0.set_ylim([0.4, 1])
@@ -144,10 +168,19 @@ if __name__ == "__main__":
         l = ax0.legend(loc='upper left')
     plot_setter(ax0, l)
 
+    major_ticks = np.arange(6, 8.5, 0.5)
+    ax1.set_xticks(major_ticks)
+    major_ticks = np.arange(0.9, 1.15, 0.05)
+    minor_ticks = np.arange(0.9, 1.11, 0.01)
+    ax1.set_yticks(major_ticks)
+    ax1.set_yticks(minor_ticks, minor=True)
+    ax1.grid(which='minor', alpha=0.2, ls='--')
+    ax1.grid(which='major', alpha=0.5)
+
     ax1.set_xlim([5.7,8])
-    ax1.set_ylim([0.95, 1.05])
+    ax1.set_ylim([0.90, 1.10])
     ax1.set_xlabel(r'log(E$_{\textrm{reco}}$/GeV)')
     ax1.set_ylabel('Ratio')
     fig.tight_layout()
-    plt.savefig(fig_dir+'event_selection/charge_test_%s.pdf' % args.selection)
+    plt.savefig(fig_dir+'systematics/%s_test_%s.pdf' % (args.param, args.selection))
     plt.close()
