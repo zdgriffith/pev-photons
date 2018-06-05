6f5cc056812429feecdc6aa1169cc365e56e8a33
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2018-06-05 13:26:49 -0500

    adding requirements and setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+def get_install_reqs(path):
+    content = read(path)
+    requirements = [req for req in content.split("\n")
+                    if req != '' and not req.startswith('#')]
+    return requirements
+
+setup(
+    name='pev_photons',
+    version='0.1',
+    description='The scripts used to produce the IceCube Gamma-Ray Analysis',
+    long_description=open('README.txt').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/zdgriffith/pev_photons',
+    author='Zachary Griffith'
+    author_email='griffitzd@gmail.com',
+    packages=find_packages(),
+    install_requires=get_install_reqs(os.path.join(here, 'requirements.txt')),
+)
