c6f77d4738771fd9cf7e2f9eefe34c432d1ab85e
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2018-09-05 20:31:44 -0500

    add hess cut off energy tests

---
+++ pev_photons/TeVCat/cluster_hess_cut_offs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+########################################################################
+# Submit a dagman to the cluster for calculating all sky trials
+########################################################################
+
+import argparse
+import os
+
+from pev_photons.utils.support import prefix, resource_dir, dag_dir
+from pev_photons.utils.cluster_support import DagMaker
+
+if __name__ == "__main__":
+
+    p = argparse.ArgumentParser(
+            description='Calculate HESS sensitivities for a given energy cut off.'
+        )
+    p.add_argument('--test', action='store_true', default=False,
+                   help='Option for running test off cluster.')
+    p.add_argument('--rm_old', action='store_true', default=False,
+                   help='Remove old dag files?')
+    args = p.parse_args()
+
+    dag_maker = DagMaker(name='hess_cut_offs', temp_dir=dag_dir)
+    if args.rm_old:
+        dag_maker.remove_old(prefix=prefix)
+
+    iters = {'source': range(15), 'Ecut':range(10, 1010, 10)}
+
+    ex = dag_maker.submit(script=os.path.join(os.getcwd(), 'hess_cut_off.py'),
+                          iters=iters,
+                          submit_file=os.path.join(resource_dir, 'basic.submit'),
+                          test=args.test, prefix=prefix)
+    os.system(ex)
---
+++ pev_photons/TeVCat/hess_cut_off.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+########################################################################
+# Calculates the sensitivity to each HESS source's
+# declination and spectral index 
+########################################################################
+
+import argparse
+import numpy as np
+
+from skylab.ps_injector import PointSourceInjector
+
+from pev_photons.utils.load_datasets import load_dataset
+from pev_photons.utils.support import prefix, resource_dir
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(
+            description='HESS source sensitivity calculation',
+            formatter_class=argparse.RawDescriptionHelpFormatter)
+    p.add_argument("--ncpu", type=int, default=1,
+                    help="Number of cores to run on.")
+    p.add_argument("--seed", type=int, default=1,
+                   help='rng seed')
+    p.add_argument("--source", type=int, default=1,
+                   help='index of source')
+    p.add_argument("--Ecut", type=int, default=1,
+                   help='Energy cut off in TeV.')
+    args = p.parse_args()
+
+    # Load the dataset.
+    ps_llh = load_dataset('point_source', ncpu=args.ncpu, seed=args.seed)
+
+    hess = np.load(prefix+'resources/hgps_sources.npz')
+    sens = list()
+
+    dec = np.radians(hess['dec'][args.source])
+    inj= PointSourceInjector(hess['alpha'][args.source], E0=1e6,
+                             Ecut=args.Ecut*1e3,
+                             sinDec_bandwidth=np.sin(np.radians(2)))
+    inj.fill(dec, ps_llh.exp, ps_llh.mc, ps_llh.livetime)
+    result = ps_llh.weighted_sensitivity([0.5], [0.9],
+                                         inj=inj,
+                                         eps=1.e-2,
+                                         n_bckg=1000,
+                                         n_iter=100,
+                                         src_ra=np.pi, src_dec=dec)
+    sens = result[0]["flux"][0]
+    np.save(prefix+'TeVCat/cut_off/{}_Ecut_{}.npy'.format(args.source, args.Ecut), sens)
