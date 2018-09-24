71150176e34ea313dc21371bd96db6037023a3e9
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2018-09-24 14:33:12 -0500

    fix missing imports

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
 import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(path, encoding='utf-8'):
+    with io.open(path, encoding=encoding) as f:
+        content = f.read()
+    return content
 
 def get_install_reqs(path):
     content = read(path)
@@ -13,10 +22,10 @@ setup(
     name='pev-photons',
     version='0.1',
     description='The scripts used to produce the IceCube Gamma-Ray Analysis',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/zdgriffith/pev-photons',
-    author='Zachary Griffith'
+    author='Zachary Griffith',
     author_email='griffitzd@gmail.com',
     packages=find_packages(),
     install_requires=get_install_reqs(os.path.join(here, 'requirements.txt')),
