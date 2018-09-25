58470a6a8e06abfbce823a77f016bbfc44a285cc
Zachary Griffith <griffitzd@gmail.com>
2018-09-25 14:12:44 -0500

    import package contents for simpler importing elsewhere

---
+++ pev_photons/utils/__init__.py
@@ -0,0 +1,5 @@
+from .support import (prefix, fig_dir, dag_dir, resource_dir, plot_style,
+                      plot_setter, ps_map, plasma_map)
+from .skymap import PolarSkyMap
+from .cluster_support import DagMaker
+from .load_datasets import load_dataset, load_systematic_dataset
