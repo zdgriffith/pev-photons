7609ac7a5388761bc45919a7efcb46632c1bebc4
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2018-11-05 13:58:39 -0600

    adding HESS energy cut off calculation

--- pev_photons/TeVCat/calc_cut_offs.py
+++ pev_photons/TeVCat/calc_cut_offs.py
@@ -12,21 +12,13 @@ from scipy import interpolate
 
 from pev_photons import utils
 
-def absorption(fname):
-    surv = np.loadtxt(os.path.join(utils.resource_dir,
-                                   'gamma_survival_vs_distance.txt'))
-    surv = surv.T
-    spline = scipy.interpolate.InterpolatedUnivariateSpline(surv[0],
-                                                            surv[1], k=2)
-    return spline
-
 def calc_flux(flux_0, alpha, E, E_0, Ecut=None, ratio=None):
         flux = flux_0*(E/E_0)**(-alpha)
         if Ecut is not None:
             flux *= np.exp(-E / Ecut)
         if ratio is not None:
             flux *= ratio
-        return flux
+        return flux[0]
 
 def crossing_point(E, flux_a, flux_b):
     sens_zone = E[np.greater(flux_a, flux_b)]
@@ -37,24 +29,30 @@ def crossing_point(E, flux_a, flux_b):
 
 if __name__ == "__main__":
 
-    hess = np.load(os.path.join(utils.resource_dir, 'hgps_sources.npz'))
+    hess = np.load(os.path.join(utils.resource_dir, 'hess_sources.npz'))
 
-    absorp_spline = absorption(utils.resource_dir+'gamma_survival_vs_distance.txt')
-    ratio = absorp_spline(hess['distance'])
-
-    Ecut = np.append(range(10, 1010, 10), range(1100, 10100, 100))
     cut_offs= np.zeros(len(hess['flux']))
     for i in range(len(hess['flux'])):
+        if i in [2, 7]:
+            continue
+        if i in [1,5]:
+            Ecut = np.array(range(100, 30100, 100))
+        else:
+            Ecut = np.array(range(100, 20100, 100))
         flux = calc_flux(hess['flux'][i]*1e-12, hess['alpha'][i],
-                         E=1e3, E_0=1,
-                         Ecut=Ecut, ratio=ratio[i])
+                         E=2e3, E_0=1,
+                         Ecut=Ecut, ratio=utils.apply_absorption(2e3, hess['distance'][i]))
 
         sens = np.zeros(Ecut.shape[0])
         for j, E_i in enumerate(Ecut):
-            flux_i = np.load(utils.prefix+'TeVCat/cut_off/{}_Ecut_{}.npy'.format(i, E_i))
-            sens[j] = calc_flux(flux_i*1e3, hess['alpha'][i], E=1e3, E_0=1e3)
+            flux_i = np.load(utils.prefix+'TeVCat/cut_off_utils/{}_Ecut_{}_sens.npy'.format(i, E_i))
+            sens[j] = calc_flux(flux_i*1e3, hess['alpha'][i], E=2e3,
+                                E_0=2e3, Ecut=Ecut,
+                                ratio=utils.apply_absorption(2e3, hess['distance'][i]))
 
         cut_offs[i] = crossing_point(Ecut, flux, sens)
+
     print(hess['name'])
     print(cut_offs)
     print(hess['distance'])
+    np.save(utils.prefix+'TeVCat/hess_cut_offs.npy', cut_offs)
