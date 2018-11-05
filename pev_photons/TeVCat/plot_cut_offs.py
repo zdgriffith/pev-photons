c0fd02d96357cacb09c8ccdd2a638cad7e29913d
Zach Griffith <zachary.griffith@icecube.wisc.edu>
2018-11-05 14:09:25 -0600

    update with new results, absorption application

--- pev_photons/TeVCat/plot_cut_offs.py
+++ pev_photons/TeVCat/plot_cut_offs.py
@@ -5,6 +5,7 @@
 # as a function of energy cut off
 ########################################################################
 
+import os
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,41 +27,49 @@ if __name__ == "__main__":
     row_n = 5
     col_n = 3
     fig, ax = plt.subplots(row_n, col_n, sharex='col', figsize=(12, 9))
-    hess = np.load(utils.resource_dir+'hgps_sources.npz')
-    hess_sens = np.load(utils.prefix+'TeVCat/hess_sens.npz')
+    hess = np.load(utils.resource_dir+'hess_sources.npz')
+    hess_sens = np.load(utils.prefix+'TeVCat/hess_sens_2pev_old.npz')
 
-    surv = np.loadtxt(utils.resource_dir+'gamma_survival_vs_distance.txt')
-    surv = surv.T
-    spline = scipy.interpolate.InterpolatedUnivariateSpline(surv[0],
-                                                            surv[1], k=2)
-    ratio = spline(hess['distance'])
-
-    E = np.arange(1, 10000, 0.1)
-    Ecut = np.append(range(10, 1010, 10), range(1100, 10100, 100))
+    E = np.arange(100, 50000, 0.1)
+    cut_offs = np.load(utils.prefix+'TeVCat/hess_cut_offs.npy')
     for i, (j, k) in enumerate(product(range(row_n), range(col_n))):
-        flux = hess['flux'][i]*(1e3)**(-hess['alpha'][i])
-        flux *= np.exp(-1e3 / E)*1e-12
-        flux *= ratio[i]
-        ax[j, k].plot(E, flux, label='H.E.S.S. Extrap.')
+        if i in [1,5]:
+            Ecut = np.array(range(100, 30100, 100))
+        else:
+            Ecut = np.array(range(100, 20100, 100))
+        continue
+        ax[j, k].set_title(hess['name'][i], fontsize=12)
+        if i in [2,7]:
+            ax[j, k].text(25, 0.5, 'N/A', fontsize=25)
+            continue
+        flux = hess['flux'][i]*(2e3)**(-hess['alpha'][i])
+        flux *= np.exp(-2e3 / E)*1e-12
+        flux *= utils.apply_absorption(2e3, hess['distance'][i])
 
         sens = []
+        disc = []
         for E_i in Ecut:
-            flux_i = np.load(utils.prefix+'TeVCat/cut_off/{}_Ecut_{}.npy'.format(i, E_i))
+            flux_i = np.load(utils.prefix+'TeVCat/cut_off_utils/{}_Ecut_{}_sens.npy'.format(i, E_i))
+            flux_i *= np.exp(-2e3 / E_i)
+            flux_i *= utils.apply_absorption(2e3, hess['distance'][i])
             sens.append(flux_i*1e3)
-
-        ax[j, k].plot(Ecut, sens, label='Sensitivity')
+ 
+        ax[j, k].plot(E*1e-3, flux, label='$\Phi_{HESS}$(2 PeV)')
+        ax[j, k].plot(Ecut*1e-3, sens, label='$\Phi_{90\%}$(2 PeV)')
+        if cut_offs[i]:
+            ax[j, k].axvline(x=cut_offs[i]*1e-3, ls=':', color='k')
+        if k == 1 and j == 3:
+            ax[j, k].legend(fontsize=16, loc='lower right')
 
         if j == (row_n-1):
-            ax[j, k].set_xlabel('Energy Cut Off [TeV]', fontsize=14)
+            ax[j, k].set_xlabel('Energy Cut Off [PeV]', fontsize=14)
         if k == 0:
-            ax[j, k].set_ylabel('Flux ', fontsize=14)
+            ax[j, k].set_ylabel('Flux at 2 PeV', fontsize=14)
         ax[j, k].set_xscale('log')
         ax[j, k].set_yscale('log')
-        ax[j, k].set_xlim([50, 10000])
-        ax[j, k].set_ylim([1e-22, 1e-15])
-        if k == 0 and j == 0:
-            ax[j, k].legend(fontsize=12, loc='upper right')
+        ax[j, k].set_xlim([0.1, 50])
+        ax[j, k].set_ylim([3e-23, 5e-19])
 
     plt.tight_layout()
-    plt.savefig(utils.fig_dir+'TeVCat/cut_offs.png', dpi=300)
+    plt.savefig(utils.fig_dir+'TeVCat/cut_offs_2pev_utils.png', dpi=300)
     plt.close()
